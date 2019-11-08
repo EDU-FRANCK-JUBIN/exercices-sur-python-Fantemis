3746d9570b932e22bd8d6e1acb0adfb51110304b
Fantemis <adel.mekki1998@hotmail.com>
2019-11-08 10:20:53 +0100

    Correction exo 3 ajout d'une condition

--- EXO-03.py
+++ EXO-03.py
@@ -2,8 +2,11 @@ import math
 nombre_sup_cinquante = int(input("Veuillez entrer un nombre superieur à 50"))
 e = 1
 list = []
-for i in range(1, nombre_sup_cinquante+1):
-    e += float(1/math.factorial(i))
-list.append(e)
-list.append(math.e - e)
-print(list)
+if(nombre_sup_cinquante > 50):
+    for i in range(1, nombre_sup_cinquante + 1):
+        e += float(1 / math.factorial(i))
+    list.append(e)
+    list.append(math.e - e)
+    print(list)
+else:
+    print("Nombre invalide, relanceez le preogramme")
